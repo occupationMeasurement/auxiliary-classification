22c25ff26c622306f6641546e382a78ba73a8161
Jan Simson <git@simson.io>
2022-08-29 17:19:24 +0200

    Update columns for auxco_mapping_from_kldb.csv

--- generate_CSVs.R
+++ generate_CSVs.R
@@ -250,52 +250,86 @@ write.csv2(
 
 
 ##############################################
-# write data to excel file, listing all kldb categories (default and folgefrage) and their associated category ids
+# Mapping of all kldb categories (default and folgefrage) to auxco_ids
 # Create file: auxco_mapping_from_kldb.csv
 ##############################################
 
-res <- NULL
+auxco_mapping_from_kldb <- NULL
 for (cat_num in seq_along(ids)) {
-  category_node <- xml_find_all(src, xpath = paste0("//klassifikation/*[", cat_num, "]"))
-  id <- xml_text(xml_find_all(category_node, xpath = "./id"))
-  kldb_id_default <- xml_attr(xml_find_all(category_node, xpath = ".//default/kldb"), "schluessel")
-  kldb_id_folgefrage <- xml_attr(xml_find_all(category_node, xpath = ".//antwort/kldb"), "schluessel")
-  res <- rbind(res, data.table(id, kldb = c(kldb_id_default, kldb_id_folgefrage)))
+  category_node <- src |>
+    xml_find_all(xpath = paste0("//klassifikation/*[", cat_num, "]"))
+  auxco_id <- xml_find_all(category_node, xpath = "./id") |>
+    xml_text()
+  title <- xml_find_all(category_node, xpath = "./bezeichnung") |>
+    xml_text()
+  kldb_id_default <- category_node |>
+    xml_find_all(xpath = ".//default/kldb") |>
+    xml_attr("schluessel")
+  kldb_id_folgefrage <- category_node |>
+    xml_find_all(xpath = ".//antwort/kldb") |>
+    xml_attr("schluessel")
+
+  auxco_mapping_from_kldb <- rbind(
+    auxco_mapping_from_kldb,
+    data.table(
+      auxco_id,
+      kldb_id = c(kldb_id_default, kldb_id_folgefrage),
+      title
+    )
+  )
 }
 
-# nicht alle kldbs stehen in der Hilfsklassifikation, denn einzelne Kategorien in der KldB unterscheiden sich kaum. Für nicht enthaltene KldBs (Spalte kldb) füge Verknüpfungen zu sehr ähnlichen kldbs (Spalte aehnlich_zu) und zu den zugehörigen IDs aus der Hilfsklassifikation (Spalte id) hinzu. Dies ermöglicht Hilfskategorien vorzuschlagen, auch wenn die jeweilige KldB nicht in der Hilfsklassifikation steht.
-res <- rbind(
-  res,
+# TODO: These manual additions should live in the XML or at least a CSV
+
+# nicht alle kldbs stehen in der Hilfsklassifikation, denn einzelne Kategorien
+# in der KldB unterscheiden sich kaum. Für nicht enthaltene KldBs (Spalte kldb)
+# füge Verknüpfungen zu sehr ähnlichen kldbs (Spalte aehnlich_zu) und zu den
+# zugehörigen IDs aus der Hilfsklassifikation (Spalte id) hinzu. Dies ermöglicht
+# Hilfskategorien vorzuschlagen, auch wenn die jeweilige KldB nicht in der
+# Hilfsklassifikation steht.
+manual_mapping_additions <- data.table(
+  auxco_id = c("1014", "1015", "1519", "1518", "5178", "5179", "1716", "1716", "2115", "1733", "1734", "1790", "3205", "3206", "3205", "3206", "3573", "3570", "3550", "3550", "3546", "3542", "7005", "3551", "3552", "3541", "7104", "7043", "7043", "7053", "1828", "1112"),
+  kldb_id = c("11183", "11183", "11402", "11402", "22182", "22182", "22183", "22184", "26382", "26383", "26383", "41383", "71382", "71382", "71383", "71383", "72214", "72214", "73282", "73283", "73284", "73293", "73293", "73293", "73293", "73293", "81382", "81783", "81784", "81784", "82283", "91484")
+)
+# Weitere KldBs stehen nicht mehr in der Hilfsklassifikation (in der
+# Ursprungsversion waren sie noch enthalten), da diese Kategorien sehr allgemein
+# gehalten sind und wir glauben, dass sich Beschäftigte im allgemeinen genauer
+# einordnen können
+manual_mapping_additions <- rbind(
+  manual_mapping_additions,
   data.table(
-    id = c("1014", "1015", "1519", "1518", "5178", "5179", "1716", "1716", "2115", "1733", "1734", "1790", "3205", "3206", "3205", "3206", "3573", "3570", "3550", "3550", "3546", "3542", "7005", "3551", "3552", "3541", "7104", "7043", "7043", "7053", "1828", "1112"),
-    kldb = c("11183", "11183", "11402", "11402", "22182", "22182", "22183", "22184", "26382", "26383", "26383", "41383", "71382", "71382", "71383", "71383", "72214", "72214", "73282", "73283", "73284", "73293", "73293", "73293", "73293", "73293", "81382", "81783", "81784", "81784", "82283", "91484") # ,
-    #                       aehnlich_zu =  c("11103", "11104", "11412", "11422", "22102", "22102", "22103", "22104", "26302", "26303", "26303", "41303", "71302", "71302", "71302", "71302", "72234", "72294", "73202", "73203", "73204", "73214", "73224", "73234", "73244", "73254", "81302", "81713", "81714", "81783", "82233", "91404")
+    auxco_id = c("2093", "2018", "1853", "2022", "2023", "2029", "2034", "1722", "1722", "9041", "9096", "9087", "9063", "9065", "9067", "9069", "9040", "9049", "9076", "9097", "9062", "9064", "9066", "9068", "9070", "9049", "9047", "9076", "4002", "4002", "4002", "4004", "4005", "4006", "4007", "4008", "4210", "4211", "4212", "4213", "4214", "1799", "1785", "6030", "1750", "3205", "3206", "3208", "3210", "3211", "3599", "3531", "3530", "3532", "3533", "3530", "3537", "3599", "5128", "5141"),
+    kldb_id = c("24202", "24202", "24202", "24202", "24202", "24202", "24202", "27103", "27104", "29202", "29202", "29202", "29202", "29202", "29202", "29202", "29202", "29203", "29203", "29203", "29203", "29203", "29203", "29203", "29203", "29204", "29204", "29204", "41203", "41283", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "42283", "42283", "62103", "62103", "71304", "71304", "71304", "71304", "71304", "71304", "73104", "73104", "73104", "73104", "73104", "73104", "73104", "93383", "93383")
   )
 )
-# Weitere KldBs stehen nicht mehr in der Hilfsklassifikation (in der Ursprungsversion waren sie noch enthalten), da diese Kategorien sehr allgemein gehalten sind und wir glauben, dass sich Beschäftigte im allgemeinen genauer einordnen können
-res <- rbind(
-  res,
+# Andere KldBs werden nicht erkannt (siehe Hilfsklassifikation),
+# weil sie abhängig von zwei Folgefragen sind
+manual_mapping_additions <- rbind(
+  manual_mapping_additions,
   data.table(
-    id = c("2093", "2018", "1853", "2022", "2023", "2029", "2034", "1722", "1722", "9041", "9096", "9087", "9063", "9065", "9067", "9069", "9040", "9049", "9076", "9097", "9062", "9064", "9066", "9068", "9070", "9049", "9047", "9076", "4002", "4002", "4002", "4004", "4005", "4006", "4007", "4008", "4210", "4211", "4212", "4213", "4214", "1799", "1785", "6030", "1750", "3205", "3206", "3208", "3210", "3211", "3599", "3531", "3530", "3532", "3533", "3530", "3537", "3599", "5128", "5141"),
-    kldb = c("24202", "24202", "24202", "24202", "24202", "24202", "24202", "27103", "27104", "29202", "29202", "29202", "29202", "29202", "29202", "29202", "29202", "29203", "29203", "29203", "29203", "29203", "29203", "29203", "29203", "29204", "29204", "29204", "41203", "41283", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "41204", "42283", "42283", "62103", "62103", "71304", "71304", "71304", "71304", "71304", "71304", "73104", "73104", "73104", "73104", "73104", "73104", "73104", "93383", "93383") # ,
-    #                       aehnlich_zu =  c("24201", "24203", "24212", "24222", "24232", "24302", "24412", "25103", "25104", "29201", "29212", "29222", "29232", "29242", "29252", "29262", "29282", "29283", "29293", "29213", "29223", "29233", "29243", "29253", "29263", "29283", "29284", "29294", "41213", "41213", "41214", "41234", "41244", "41254", "41264", "41274", "41284", "41284", "41284", "41284", "41284", "42203", "34313", "62102", "62193", "71303", "71303", "71314", "71394", "71394", "71594", "73124", "73134", "73144", "73154", "73183", "73194", "71394", "93303", "93393")
-  )
-)
-# Andere KldBs werden nicht erkannt (siehe Hilfsklassifikation), weil sie abhängig von zwei Folgefragen sind
-res <- rbind(
-  res,
-  data.table(
-    id = c("1748", "1749", "1748", "1749"),
-    kldb = c("61204", "61214", "61284", "61284")
+    auxco_id = c("1748", "1749", "1748", "1749"),
+    kldb_id = c("61204", "61214", "61284", "61284")
   )
 )
 
+# Add title information and then combine the manual mapping
+# with the existing auxco mapping
+manual_mapping_additions <- merge(
+  manual_mapping_additions,
+  auxco_mapping_from_kldb[, list(auxco_id, title)],
+  by = "auxco_id"
+)
+auxco_mapping_from_kldb <- rbind(
+  auxco_mapping_from_kldb,
+  manual_mapping_additions
+)
 
-res <- unique(res)
+# TODO: Add kldb_title via join
 
-map_kldb_to_auxcoid <- res
+auxco_mapping_from_kldb <- unique(auxco_mapping_from_kldb)
+
 write.csv2(
-  res,
+  auxco_mapping_from_kldb,
   row.names = FALSE,
   file = file.path(output_dir, "auxco_mapping_from_kldb.csv"),
   fileEncoding = "UTF-8"
